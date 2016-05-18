ede96c79ce1be2988f863de7cb5e0f1a57953b6d
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2016-05-18 13:58:28 +0200

    Refactor test

--- src/ittest/java/ndr/brt/TimbroServiceITest.java
+++ src/ittest/java/ndr/brt/TimbroServiceITest.java
@@ -4,28 +4,37 @@ import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.server.Server;
 import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static ndr.brt.Application.*;
+import static ndr.brt.Application.initServer;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TimbroServiceITest {
 
+    private Server server;
+    private HttpClient client;
+
+    @Before
+    public void setUp() throws Exception {
+        server = initServer();
+        server.start();
+        client = new HttpClient();
+        client.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+        client.stop();
+    }
+
     @Test
     public void send_and_retrieve() throws Exception {
-        Server server = initServer();
-        server.start();
-
-        HttpClient client = new HttpClient();
-        client.start();
-
         ContentResponse response = client.GET("http://localhost:8080/");
 
-        MatcherAssert.assertThat(response.getContentAsString(), CoreMatchers.is("Segnato"));
-
-        client.stop();
-
-        server.stop();
+        assertThat(response.getContentAsString(), CoreMatchers.is("Segnato"));
     }
 
 }
